802bf5f413c942d48d1568286183223243fe74bf
yelsino <yelsino321@gmail.com>
2025-09-08 09:32:30 -0500

    Update recent

--- dist/data/categorias.js
+++ dist/data/categorias.js
@@ -5,16 +5,40 @@ exports.CATEGORIAS = [
     {
         id: "1",
         nombre: "verduras",
-        tag: "verdura"
+        tag: "verdura",
+        descripcion: "verduras",
+        icono: "verdura",
+        color: "verdura",
+        orden: 1,
+        activa: true,
+        fechaCreacion: "2021-01-01",
+        subcategorias: [],
+        imagen: "verdura"
     },
     {
         id: "2",
         nombre: "frutas",
-        tag: "fruta"
+        tag: "fruta",
+        descripcion: "frutas",
+        icono: "fruta",
+        color: "fruta",
+        orden: 2,
+        activa: true,
+        fechaCreacion: "2021-01-01",
+        subcategorias: [],
+        imagen: "fruta"
     },
     {
         id: "3",
         nombre: "tuberculos",
-        tag: "tuberculo"
+        tag: "tuberculo",
+        descripcion: "tuberculos",
+        icono: "tuberculo",
+        color: "tuberculo",
+        orden: 3,
+        activa: true,
+        fechaCreacion: "2021-01-01",
+        subcategorias: [],
+        imagen: "tuberculo"
     }
 ];
